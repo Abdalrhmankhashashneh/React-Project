8852518a065551509f97f78a99cedbb88ee1678d
yousefalhindawi <eng.yousefalhindawi@gmail.com>
2022-07-06 15:29:52 +0300

    register

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -1,8 +1,9 @@
 import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import { AppContext } from "../Hooks/appContext";
 
 export default function Login() {
-    const { state, setState } = useContext(AppContext);
+    const { userProvider, registerHandler } = useContext(AppContext);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -17,35 +18,58 @@ export default function Login() {
     const handelSubmit = (e) => {
         e.preventDefault();
         console.log(username, password);
-        if (username === state.user.email && password === state.user.password) {
-            setState({
-                ...state,
+        if (username === userProvider.user.email && password === userProvider.user.password) {
+            userProvider.setUser({
+                ...userProvider.user,
                 isLoggedIn: true
             });
             alert("You are logged in");
         }
         else {
-            setState({
-                ...state,
+            userProvider.setUser({
+                ...userProvider.user,
                 isLoggedIn: false,
             });
             alert("You are not logged in");
         }
     }
     return (
-        <div className="container mt-5 col ">
-
-            <div className="container mt-5 col " >
-                <h1>Login</h1>
-
-                <form onSubmit={handelSubmit}>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" name="username" onChange={handelChange} />
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" onChange={handelChange} name="password" />
-                    <button type="submit">Submit</button>
-                </form>
+        <section className=" bg-image my-5"
+        style={{ backgroundImage: "url('assets/img/about.jpg')" }}>
+        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+                <div className="card" style={{ borderRadius: "15px" }}>
+                  <div className="card-body p-5">
+                    <h2 className="text-uppercase text-center mb-5" style={{ color: "#cda45e" }}>Login</h2>
+      
+                    <form>
+                      <div className="form-outline mb-3">
+                        <label className="form-label " htmlFor="form3Example3cg" style={{ color: "#cda45e" }}>Your Email</label>
+                        <input name="email" type="email" id="form3Example3cg" className="form-control form-control-lg" value={userProvider.userState.user.email} onChange={handelChange}/>
+                      </div>
+                      <div className="form-outline mb-3">
+                        <label className="form-label " htmlFor="form3Example4cg" style={{ color: "#cda45e" }}>Password</label>
+                        <input name="password" type="password" id="form3Example4cg" className="form-control form-control-lg" value={userProvider.userState.user.password} onChange={handelChange}/>
+                      </div>
+      
+                      <div className="d-flex justify-content-center">
+                        <button type="button"
+                          className="login-a-table-btn fw-bold">Login</button>
+                      </div>
+      
+                      <p className="text-center mt-5 mb-0" style={{ color: "#cda45e" }}>Doesn't have an account? <Link to="/register"
+                          className="fw-bold" style={{ color: "#cda45e" }}><u>Register</u></Link></p>
+      
+                    </form>
+      
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
+      </section>
     );
 } //end Login
--- Frontend/src/pages/Register.js
+++ Frontend/src/pages/Register.js
@@ -1,19 +1,37 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { AppContext } from "../Hooks/appContext";
-export default function Register() {
-    const { state, setState } = useContext(AppContext);
+ const Register =()=> {
+
+    // const [registerInput, setregisterInput] = useState({
+    //         name: "",
+    //         email: "",
+    //         phone: "",
+    //         password: "",
+    //         confirmPassword: ""
+    // });
+
+    // const handelChange = (e) => {
+    //     setregisterInput({ ...registerInput, [e.target.name]: e.target.value });
+    // };
+
+    const { userProvider, registerHandler} = useContext(AppContext);
+    
     const handelChange = (e) => {
-        setState({
-            ...state,
+        userProvider.setUser({
+            ...userProvider.userState,
+            // isLoggedIn: true,
             user: {
-                ...state.user,
+                ...userProvider.userState.user,
                 [e.target.name]: e.target.value
             }
         });
     }
     const handelSubmit = (e) => {
         e.preventDefault();
-        console.log(state.user);
+        registerHandler(userProvider.userState)
+        console.log(userProvider.userState);
+        console.log(userProvider.userState.isLoggedIn);
     }
 
 
@@ -21,55 +39,55 @@ export default function Register() {
     return (
 
 <section className=" bg-image my-5"
-  style={{ backgroundImage: "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')" }}>
+  style={{ backgroundImage: "url('assets/img/about-bg.jpg')", backgroundColor: "rgba(255,255,255)"}}>
   <div className="mask d-flex align-items-center h-100 gradient-custom-3">
     <div className="container h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
         <div className="col-12 col-md-9 col-lg-7 col-xl-6">
           <div className="card" style={{ borderRadius: "15px" }}>
             <div className="card-body p-5">
-              <h2 className="text-uppercase text-center mb-5 text-dark">Create an account</h2>
+              <h2 className="text-uppercase text-center mb-5" style={{ color: "#cda45e" }}>Create an account</h2>
 
-              <form>
+              <form onSubmit={handelSubmit}>
 
                 <div className="form-outline mb-3">
-                  <input name="name" type="text" id="form3Example1cg" className="form-control form-control-lg" value={state.user.name}  onChange={handelChange}/>
-                  <label className="form-label text-dark" htmlFor="form3Example1cg">Your Name</label>
+                  <label className="form-label" htmlFor="form3Example1cg" style={{ color: "#cda45e" }}>Your Name</label>
+                  <input name="name" type="text" id="form3Example1cg" className="form-control form-control-lg" value={userProvider.userState.user.name}  onChange={handelChange}/>
                 </div>
 
                 <div className="form-outline mb-3">
-                  <input name="email" type="email" id="form3Example3cg" className="form-control form-control-lg" value={state.user.email} onChange={handelChange}/>
-                  <label className="form-label text-dark" htmlFor="form3Example3cg">Your Email</label>
+                  <label className="form-label" htmlFor="form3Example3cg" style={{ color: "#cda45e" }}>Your Email</label>
+                  <input name="email" type="email" id="form3Example3cg" className="form-control form-control-lg" value={userProvider.userState.user.email} onChange={handelChange}/>
                 </div>
                 <div className="form-outline mb-3">
-                  <input name="phone" type="text" id="form3Example5cg" className="form-control form-control-lg" value={state.user.phone} onChange={handelChange}/>
-                  <label className="form-label text-dark" htmlFor="form3Example5cg">Your Phone</label>
-                </div>
-
-                <div className="form-outline mb-3">
-                  <input name="password" type="password" id="form3Example4cg" className="form-control form-control-lg" value={state.user.password} onChange={handelChange}/>
-                  <label className="form-label text-dark" htmlFor="form3Example4cg">Password</label>
+                  <label className="form-label" htmlFor="form3Example5cg" style={{ color: "#cda45e" }}>Your Phone</label>
+                  <input name="phone" type="text" id="form3Example5cg" className="form-control form-control-lg" value={userProvider.userState.user.phone} onChange={handelChange}/>
                 </div>
 
                 <div className="form-outline mb-3">
-                  <input name="repassword"type="password" id="form3Example4cdg" className="form-control form-control-lg" value={state.user.repassword} onChange={handelChange}/>
-                  <label className="form-label text-dark" htmlFor="form3Example4cdg">Repeat your password</label>
+                  <label className="form-label" htmlFor="form3Example4cg" style={{ color: "#cda45e" }}>Password</label>
+                  <input name="password" type="password" id="form3Example4cg" className="form-control form-control-lg" value={userProvider.userState.user.password} onChange={handelChange}/>
+                </div>
+
+                <div className="form-outline mb-3">
+                  <label className="form-label" htmlFor="form3Example4cdg" style={{ color: "#cda45e" }}>Repeat your password</label>
+                  <input name="confirmPassword" type="password" id="form3Example4cdg" className="form-control form-control-lg" value={userProvider.userState.user.confirmPassword} onChange={handelChange}/>
                 </div>
 
                 <div className="form-check d-flex justify-content-center mb-5">
                   <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3cg"  onChange={handelChange}/>
-                  <label className="form-check-label text-dark" htmlFor="form2Example3g">
-                    I agree all statements in <a href="#!" className="text-body"><u>Terms of service</u></a>
+                  <label className="form-check-label" htmlFor="form2Example3cg" style={{ color: "#cda45e" }}>
+                    I agree all statements in <a href="#!" className=""><u>Terms of service</u></a>
                   </label>
                 </div>
 
                 <div className="d-flex justify-content-center">
-                  <button type="button"
-                    className="btn btn-outline-dark btn-block btn-lg gradient-custom-4 ">Register</button>
+                  <button type="submit"
+                    className="login-a-table-btn fw-bold">Register</button>
                 </div>
 
-                <p className="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!"
-                    className="fw-bold text-body"><u>Login here</u></a></p>
+                <p className="text-center mt-5 mb-0" style={{ color: "#cda45e" }}>Have already an account? <Link to="/login"
+                    className="fw-bold"><u>Login</u></Link></p>
 
               </form>
 
@@ -100,3 +118,6 @@ export default function Register() {
         // </div>
     );
 } //end Register
+
+
+export default Register;
