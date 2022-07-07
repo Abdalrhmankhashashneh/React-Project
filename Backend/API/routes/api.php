3bfdd2b4ccec773e76e7504f0b17e09c13a97677
Abood kh <abdalrhmankhashasneh@gmail.com>
2022-07-07 09:14:37 +0300

    uploads edits in registers

--- Backend/API/routes/api.php
+++ Backend/API/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\User_c;
+use App\Http\Controllers\Auth_c;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('users/register', [Auth_c::class ,'register']);
 Route::get('users/search/{name}', [User_c::class, 'search']);
 Route::group(['middleware' => [ 'auth:sanctum' ]], function () {
-    Route::resource('users', User_c::class);
+    Route::post('users/logout', [Auth_c::class ,'logout']);
 });
