8af5fcdd63e52c2b4b9c5f9f836157ef1573741d
hayaabdallah <100359839+hayaabdallah@users.noreply.github.com>
2022-07-08 14:51:49 +0100

    edit

--- Frontend/src/pages/Floor.js
+++ Frontend/src/pages/Floor.js
@@ -25,17 +25,17 @@
 //           fetching();
 //         }, [])
 
-        
-  
+
+
 //         const [List, setList] = useState([]);
 
 //         const [selectedCategory, setSelectedCategory] = useState();
-      
+
 //         // Add default value on page load
 //         useEffect(() => {
 //           setList(api);
 //         }, []);
-      
+
 //         // Function to get filtered list
 //         function getFilteredList() {
 //           // Avoid filter when selectedCategory is null
@@ -44,14 +44,14 @@
 //           }
 //           return List.filter((item) => item.category === selectedCategory);
 //         }
-      
+
 //         // Avoid duplicate function calls with useMemo
 //         var filteredList = useMemo(getFilteredList, [selectedCategory, List]);
-      
+
 //         function handleCategoryChange(event) {
 //           setSelectedCategory(event.target.value);
 //         }
-   
+
 //     return (
 //         <div>
 //           <div>
@@ -79,51 +79,78 @@
 //         </div>
 //       </section>
 //     </div>
-    
+
 //       )
 // }
 
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 function Floor() {
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     window.scroll({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-      })
-},[])
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }, [])
   const { id } = useParams();
   const [floor, setFloor] = useState([]);
-  const [category ,setCategory] =useState([]);
+  const [category, setCategory] = useState([]);
   useEffect(() => {
     const fetching = async () => {
       const api = await fetch(`http://127.0.0.1:8000/api/floor_type/${id}`);
       const dataa = await api.json();
       setCategory(dataa);
-  
+
     }
     fetching();
   }, [])
   console.log(floor)
 
   return (
-    <div className="col-lg-4 " >
-    { category.map((floor) => (
 
-     
-      <div className="card-body">
-        {/* <img src={'https://image.tmdb.org/t/p/w500/' + floor.backdrop_path} className="card-img-top" alt="Card Image" /> */}
-        <h5 className="card-title">{floor.name}</h5>
-        <p className="card-text">{floor.id}</p>
-        {/* <p className="card-text">{floor.vote_average}</p> */}
-      </div>
+    // <div className="col-lg-4 " >
+    // { category.map((floor) => (
 
-    )) }
+
+    //   <div className="card-body">
+    //     {/* <img src={'https://image.tmdb.org/t/p/w500/' + floor.backdrop_path} className="card-img-top" alt="Card Image" /> */}
+    //     <h5 className="card-title">{floor.name}</h5>
+    //     <p className="card-text">{floor.id}</p>
+    //     {/* <p className="card-text">{floor.vote_average}</p> */}
+    //   </div>
+
+    // )) }
+    //   </div>
+    <section id="why-us" className="why-us">
+      <div className="container" data-aos="fade-up">
+
+        <div className="section-title">
+
+        </div>
+
+        <div className="row">
+
+
+          {category.map((floor) => (
+
+            <div className="col-lg-4" style ={{marginTop:'30px'}}>
+              <div className="box" data-aos="zoom-in" data-aos-delay="100">
+                <h4>Table number : {floor.name}</h4>
+                <p>Number of seats : {floor.people_count}</p>
+                <p>Price : {floor.price} $</p>
+                <Link to={'/floor/'} className="btn btn mt-auto align-self-start" style={{backgroundColor:'#cda45e',marginTop:'30px'}}>Book Now</Link>
+              </div>
+            </div>
+
+
+
+          ))}
+        </div>
       </div>
-    
+    </section>
 
   )
 
