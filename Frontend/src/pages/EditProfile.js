3e07788a3967af73b75eff6ee4ab19f1018a536d
Marahalkhateeb12 <marahalkhateeb12@gmail.com>
2022-07-16 20:08:47 +0300

    main

--- Frontend/src/pages/EditProfile.js
+++ Frontend/src/pages/EditProfile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
